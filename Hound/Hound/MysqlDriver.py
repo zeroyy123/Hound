bbdc8e536e7c3300e232314a5695b55d8949ecb9
zeroyy123 <yujiachi163@163.com>
2016-08-21 14:19:15 +0800

    Sql driver finish ,next step to finish auto capture

--- Hound/Hound/MysqlDriver.py
+++ Hound/Hound/MysqlDriver.py
@@ -12,7 +12,7 @@ class MysqlDriver():
     def create_table(self,table_name,df):
         SQL = 'drop table if exists '+table_name
         self.cursor.execute(SQL)
-
+        df = df.dropna()
         columns = df.columns
         SQL = 'create table '+table_name+'('
         for i in range(len(columns)):
@@ -39,31 +39,99 @@ class MysqlDriver():
         # 'insert into user values(%s,%s),value
         SQL = 'insert into ' + table_name + ' values('
         columns = df.columns
-        df = df.fillna('')
+        for i in range(len(columns)):
+            temp = df[columns[i]]
+            temp = temp.dropna()
+            if (type(temp.values[0]) is unicode) or (type(temp.values[0]) is str):
+                df[columns[i]] = df[columns[i]].fillna('')
+            else:
+                df[columns[i]] = df[columns[i]].fillna(0)
         for j in range(len(columns)):
             if j < len(columns)-1:
                 SQL = SQL + '%s,'
             else:
                 SQL = SQL + '%s)'
         print SQL
-        columns = df.columns
-        for i in range(1):
+
+
+        temp = df.values.tolist()
+        print len(temp)
+
+        print type(temp)
+        print 'writing...'
+
+        delta = 10000
+        l1 = len(temp)/delta
+        l2 = len(temp)%delta
+        for i in range(l1):
             print i
-            print df.values[i]
-            self.cursor.execute(SQL,df.values[i])
+            self.cursor.executemany(SQL,temp[i*delta:(i+1)*delta])
+        self.cursor.executemany(SQL,temp[l1*delta:len(temp)])
+        # for i in range(len(df)):
+        #     if i % 100 == 0:
+        #         print i
+        #     # print df.values[i]
+        #     self.cursor.execute(SQL,df.values[i])
 
 
+def get_name():
+    df = pd.read_csv(U'F:/GitHub/Hound_data/item_list.csv')
+    df = df.fillna('')
+
+    L = len(df)
+    item_catepory = []
+    for i in range(L):
+        catepory = (df['cate_1'].values)[i]
+        catepory = catepory.replace(' ','')
+        catepory = catepory.replace("'",'')
+        catepory = catepory.replace('&','')
+        catepory = catepory.replace('/','_')
+        catepory = catepory.replace('"','')
+        catepory = catepory.replace('*','x')
+        catepory = catepory.replace('<','')
+        catepory = catepory.replace('>','')
+        item_catepory.append(catepory)
+
+    dout = []
+    for i in range(len(item_catepory)):
+        if i == 0:
+            dout.append(item_catepory[i])
+        elif item_catepory[i] != item_catepory[i-1]:
+            dout.append(item_catepory[i])
+    return dout
 
 if __name__ == '__main__':
-    name = 'LightsLighting'
-    # writer = pd.ExcelWriter(U'F:/GitHub/category/' + name+'.xlsx', engine='xlsxwriter')
-    df = pd.read_excel(U'F:/GitHub/category/' + name+'.xlsx','Sheet1',index_col=None,na_values=np.nan)
-    del df['Unnamed: 0']
-    del df['count']
-    del df['elem_ship']
-    # df = df[['elem_ID','elem_feedback']]
-    # for i in range(len(df.columns)):
-    #     print df.columns[i],type(df[df.columns[i]].values[0])
+    names = get_name()
+    print names
+
     MD = MysqlDriver()
-    MD.create_table(name,df)
-    MD.insert_data(name,df)
+    for n in range(8,len(names),1):
+        name = names[n]
+        print name
+        # writer = pd.ExcelWriter(U'F:/GitHub/category/' + name+'.xlsx', engine='xlsxwriter')
+
+        try:
+            df = pd.read_excel(U'F:/GitHub/category/' + name+'.xlsx','Sheet1',index_col=None,na_values=np.nan)
+        except:
+            df = pd.read_excel(U'F:/GitHub/category/' + name+'_1.xlsx','Sheet1',index_col=None,na_values=np.nan)
+            df = df.append(pd.read_excel(U'F:/GitHub/category/' + name+'_2.xlsx','Sheet1',index_col=None,na_values=np.nan))
+        del df['Unnamed: 0']
+        del df['count']
+        elem_ship = df['elem_ship'].values
+        for i in range(len(elem_ship)):
+            elem_ship[i] = elem_ship[i].replace('/','')
+            elem_ship[i] = elem_ship[i].replace('\t','')
+            elem_ship[i] = elem_ship[i].replace('\r','')
+            elem_ship[i] = elem_ship[i].replace('\n','')
+            elem_ship[i] = elem_ship[i].replace(' ','')
+        df['elem_ship'] = elem_ship
+        columns = df.columns
+        for i in range(len(columns)):
+            temp = df[columns[i]]
+            temp = temp.dropna()
+            if len(temp) == 0:
+                del df[columns[i]]
+
+        name = name.replace(',','')
+        MD.create_table(name,df)
+        MD.insert_data(name,df)
